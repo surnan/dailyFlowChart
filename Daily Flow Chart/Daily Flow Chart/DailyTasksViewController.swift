052b8531eb387a5b02c56c4a279d379c06cc42bd
surnan <forsuresh@yandex.com>
2018-03-06 21:33:38 -0500

    Project created and starts without storyboard.  Opening VC is UINavigationController

---
+++ Daily Flow Chart/Daily Flow Chart/DailyTasksViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DailyTasksViewController.swift
+//  Daily Flow Chart
+//
+//  Created by admin on 3/6/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import UIKit
+
+class DailyTasksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        view.backgroundColor = UIColor.white
+        super.viewDidLoad()
+    }
+}
+
