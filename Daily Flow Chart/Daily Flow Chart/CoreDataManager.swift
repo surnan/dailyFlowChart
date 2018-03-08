af50762d7563c040b5a018a6fd5e2c43d64bccc4
surnan <forsuresh@yandex.com>
2018-03-08 10:19:39 -0500

    CoreData manually added.  However CoreData functionality not successfully implemented yet.

---
+++ Daily Flow Chart/Daily Flow Chart/CoreDataManager.swift
@@ -0,0 +1,38 @@
+//
+//  CoreDataManager.swift
+//  Daily Flow Chart
+//
+//  Created by admin on 3/8/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+struct CoreDataManager {
+    static let shared = CoreDataManager()
+    
+    let persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "TaskModelCoreData")
+        container.loadPersistentStores(completionHandler: { (_, err) in
+            if let err = err {
+                fatalError("Unable to persistent container: \(err)")
+            }
+        })
+        return container
+    }()
+    
+    func fetchData() -> [Task] {
+        let context = CoreDataManager.shared.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<Task>(entityName: "Task")
+        
+        
+        do {
+            let taskArray = try context.fetch(fetchRequest)
+            taskArray.forEach { print($0.name ?? "")}
+            return taskArray
+        } catch let fetchErr {
+            fatalError("Fetch Request Error: \(fetchErr)")
+        }
+    }
+}
