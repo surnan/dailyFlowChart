0a8f67e1e89e4be02ef9ed9b501da8b45f963d7b
surnan <forsuresh@yandex.com>
2018-03-10 12:54:26 -0500

    extension on UINavigationController that make top icons on screen white (cell phone provider, signal strength, clock)

---
+++ Daily Flow Chart/Daily Flow Chart/Cutomizations.swift
@@ -0,0 +1,15 @@
+//
+//  Cutomizations.swift
+//  Daily Flow Chart
+//
+//  Created by admin on 3/10/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import UIKit
+
+extension UINavigationController {
+    open override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
